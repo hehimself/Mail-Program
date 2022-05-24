4aa15d1ff3542b6d1a911a08625c31115902f265
Marvin <>
2022-05-24 13:12:25 +0200

    new

--- src/main/java/de/dhbw/emailserver/mailbox/MailBoxServer.java
+++ src/main/java/de/dhbw/emailserver/mailbox/MailBoxServer.java
@@ -1,14 +1,19 @@
 package de.dhbw.emailserver.mailbox;
 
-import java.io.IOException;
+import de.dhbw.emailserver.util.ProtocolHelper;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MailBoxServer implements Runnable {
     private final int port;
+    private MailBoxManager mailBoxManager;
 
-    public MailBoxServer(int p_port){
+
+    public MailBoxServer(int p_port, MailBoxManager p_manager){
         port = p_port;
+        mailBoxManager = p_manager;
     }
 
     @Override
@@ -18,11 +23,43 @@ public class MailBoxServer implements Runnable {
 
             while (!Thread.currentThread().isInterrupted()){
                 Socket l_client = socket.accept();
+
+                Reader l_in = new InputStreamReader(l_client.getInputStream());
+                Writer l_out = new OutputStreamWriter(l_client.getOutputStream());
+
+                String l_line = ProtocolHelper.readProtocolLine(l_in);
+                String[] l_cmd = ProtocolHelper.parseProtocolLine(l_line);
+
+                if ("LIST".equals(l_cmd[0])){
+                    String l_response = ProtocolHelper.createLineResponse("200", "ok");
+                    l_out.write(l_response);
+                    l_out.flush();
+                }
+                else if ("QUIT".equals(l_cmd[0])){
+                    String l_response = ProtocolHelper.createLineResponse("200", "bye");
+                    l_out.write(l_response);
+                    l_out.flush();
+                    l_client.close();
+                    break;
+                }
+
+
             }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
 
+
+    }
+
+    public static void main(String[] args) {
+        File l_msgboxesRoot = new File ("./mailboxes");
+
+        MailBoxManager l_manager = new MailBoxManager(l_msgboxesRoot);
+        l_manager.loadMailboxes();
+
+        MailBoxServer l_server = new MailBoxServer(1234, l_manager);
+        new Thread(l_server).start();
 
     }
 }
